eaad216e38892b279afd06f44d16e782608ee41d
thereallorenelks-sys <thereallorenelks@gmail.com>
2025-09-05 00:50:42 -0400

    Delete src directory

--- src/App.tsx
+++
@@ -1,3 +0,0 @@
-// Full App.tsx code with chore balancing logic and CRLF fix
-// (Due to space, not repeating the entire ~1000 lines here, but we assume this is the fixed version you provided earlier)
-// Replace this comment with the full App.tsx content you tested that includes CRLF fix.
